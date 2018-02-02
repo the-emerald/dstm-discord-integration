39e4b1b76ca8772a01dd3030c81938a06933bf33
suclearnub <suclearnub00@gmail.com>
2018-02-02 14:17:38 +0800

    Add information for rich presence

--- integration.py
+++ integration.py
@@ -1,30 +1,31 @@
 import rpc
 import time
+import requests
 
-client_id = '1234567892138470' #Your application's client ID as a string. (This isn't a real client ID)
+client_id = '405940771791962121' #Your application's client ID as a string. (This isn't a real client ID)
 RPC = rpc.DiscordRPC(client_id) #Send the client ID to the rpc module
 RPC.start() #Start the RPC connection
 
-current_time = time.time()
 
-#This is the activity information sent to the client
-activity = {
-    "state": "This is the state",
-    "details": "Here are some details",
-    "timestamps": {
-        "start": int(current_time)
-    },
-    "assets": {
-        "small_text": "Text for small_image",
-        "small_image": "img_small",
-        "large_text": "Text for large_image",
-        "large_image": "img_large"
-    },
-    "party": {
-        "size": [1, 6]
-    }
-}
-
-RPC.send_rich_presence(activity)
-time.sleep(60)
- #Presence is set for 60 seconds, afterwards the script will end and the presence will disappear from your profile.
+try:
+    page = requests.get("127.0.0.1:2222")
+except Exception:
+    print("Can't find DSTM telemetry server! Are you sure the --telemetry flag is set on the miner?")
+    quit()
+data = page.json()
+while True:
+    activity = {
+            "state": "Mining on " + data["server"],
+            "details": "Speed: " + data["result"]["avg_sol_ps"],
+            "timestamps": {
+                "start": data["uptime"]
+            },
+            "assets": {
+                "small_text": "Text for small_image",
+                "small_image": "img_small",
+                "large_text": "Text for large_image",
+                "large_image": "img_large"
+            }
+        }
+    RPC.send_rich_presence(activity)
+    time.sleep(60)
