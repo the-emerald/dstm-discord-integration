8f2feb1fc0d23665d327d5360cd90de8ce9ab5d4
Anson <git@anson-cheung.com>
2018-02-03 00:51:23 +0800

    Switch over to alternative RPC library

--- integration.py
+++ integration.py
@@ -7,41 +7,37 @@ def fetch_data():
     try:
         page = requests.post("http://127.0.0.1:2222/jsonrpc")
         data = page.json() # This will most likely never work
-        can_post_normally = True
     except Exception as e:
         # Because requests doesn't like what DSTM outputs and always throws a BadStatusLine,
         # we'll just catch every single exception here - by doing string manipulation later
         # it'll be clear if JSON was passed or some garbage.
         message = str(e)
-        can_post_normally = False
+    try:
+        data = message.split("(")[2][1:-6]
+        return data
+    except Exception:
+        print("Can't find DSTM telemetry data! Are you sure the --telemetry flag is set on the miner? Is the miner on?")
+        quit()
 
-    if not can_post_normally:
-        try:
-            data = message.split("(")[2][1:-6]
-        except Exception:
-            print("Can't find DSTM telemetry data! Are you sure the --telemetry flag is set on the miner? Is the miner on?")
-            quit()
-
-    return data
-
+print("DSTM Miner Integration for Discord")
+print("Written by suclearnub\n")
 client_id = '405940771791962121' #Your application's client ID as a string. (This isn't a real client ID)
-RPC = rpc.DiscordRPC(client_id) #Send the client ID to the rpc module
-try:
-    RPC.start() #Start the RPC connection
-    print("RPC Connection Success.")
-except FileNotFoundError:
-    print("Error: Discord has not been started yet. Please make sure Discord is active.")
-    quit()
+rpc_obj = rpc.DiscordIpcClient.for_platform(client_id) #Send the client ID to the rpc module
+print("RPC connection successful.")
+
 time.sleep(5)
+start_time = time.time()
 while True:
-    data = json.loads(fetch_data())
-    #print(data['result'])
-    #print(data['result'][0]['gpu_id'])
+    try:
+        data = json.loads(fetch_data())
+    except Exception:
+        print("Can't find DSTM telemetry data! Are you sure the --telemetry flag is set on the miner? Is the miner on?")
+        quit()
     activity = {
             "state": "Mining on " + data["server"],
-            "details": "Speed: " + str(data["result"][0]["avg_sol_ps"]),
+            "details": "Speed: " + "{:.0f}".format(data["result"][0]["avg_sol_ps"]) + " Sol/s",
             "timestamps": {
-                "start": data["uptime"]
+                "start": start_time
             },
             "assets": {
                 "small_text": "Text for small_image",
@@ -50,6 +46,5 @@ while True:
                 "large_image": "img_large"
             }
         }
-    RPC.send_rich_presence(activity)
-    print("Rich presence sent.")
-    time.sleep(60)
+    rpc_obj.set_activity(activity)
+    time.sleep(30)
